1befc53fd15226f23fcd423000406652fc820ae7
arieuger <antiaroig@gmail.com>
2018-03-16 06:39:48 +0100

    Update buscarcomentarios.php

--- PHP/buscarcomentarios.php
+++ PHP/buscarcomentarios.php
@@ -1,10 +1,10 @@
-﻿<?php
+<?php
 header('Content-type: text/xml');
 require("bd.php");
 
 if(isset($_GET['idproducto'])) {
 	$sql = "SELECT * FROM comentario JOIN usuario ON comentario.usuario_idusuario = usuario.idusuario WHERE";
-		$sql .= " comentario.producto_idproducto = '%$_GET[idproducto]%';";
+		$sql .= " comentario.producto_idproducto = '" . $_GET[idproducto] . "';";
 }
 else {
 	die ("<erro>Falta o id de producto</erro>");
@@ -16,8 +16,8 @@ $resultado = $bd->query($sql);
 echo "<comentarios>";
 while($fila = $resultado->fetch_array()){
 	echo "<comentario";
-	echo " usuario='".$fila['usuario.nick'] . "'";
-	echo " texto='".$fila['comentario.texto'] . "'";
+	echo " usuario='".$fila['nick'] . "'";
+	echo " texto='".$fila['texto'] . "'";
 	echo "/>";
 }
 echo "</comentarios>";
