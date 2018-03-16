b69965b827d28ea343756adf778bcd1a7a002555
arieuger <antiaroig@gmail.com>
2018-03-16 05:13:49 +0100

    Add files via upload

---
+++ PHP/inserir_produto.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+header('Content-type: text/xml');
+
+if( ! isset($_GET['idtenda']) ){
+	echo "<erro mensaxe='Falta o parámetro idtenda' >Erro ó inserir o produto</erro>";
+	exit();
+}
+
+if( ! isset($_GET['nome']) ){
+	echo "<erro mensaxe='Falta o parámetro nome' >Erro ó inserir o produto</erro>";
+	exit();
+}
+
+if( ! isset($_GET['descript']) ){
+	echo "<erro mensaxe='Falta o parámetro descript' >Erro ó inserir o produto</erro>";
+	exit();
+}
+
+if( ! isset($_GET['prezo']) ){
+	echo "<erro mensaxe='Falta o parámetro prezo' >Erro ó inserir o produto</erro>";
+	exit();
+}
+
+require("bd.php");
+
+
+$sql =  "SELECT usuario.id FROM usuario WHERE  usuario.usuario = '" .$_SESSION['usuario'] . "';";
+//echo $sql;
+
+$resultado = $bd->query($sql);
+
+$fila = $resultado->fetch_array();
+
+
+$sql  = "INSERT INTO producto (nome, descript, urlFoto) ";
+$sql .= "VALUES ('" . $_GET['nome'] . "', '" . $_GET['descript'] . "', '');";
+
+//echo $sql;
+
+if($bd->query($sql)) {
+
+    $sql = "SELECT last_insert_id() as id";
+   
+//echo $sql;
+    $resultado = $bd->query($sql);
+	$fila = $resultado->fetch_array();
+	
+	$sql  = "INSERT INTO tenda_producto (tenda_idtenda, producto_idproducto, prezo) ";
+    $sql .= "VALUES ('" . $_GET['idtenda'] . "', '" . $fila['id'] . "', '". $_GET['idtenda'] . "');";
+	
+} 
+?>
